181ad98d0fc05068a088e21abb54d8784748f383
Sergey Bizinow <44430947+biz92@users.noreply.github.com>
2019-01-05 20:30:03 +0300

    Update 4th.py

--- 4th.py
+++ 4th.py
@@ -1,1 +1,9 @@
+my_list = ['peaches', 'tomatoes', 'coconut', 'apple', 'cat']
 
+my_str = ' '
+
+for i in range(len(my_list) - 1):
+    my_list[i]
+    my_str += my_list[i] + ","
+
+print(my_str + 'and ' + my_list[-1] )
