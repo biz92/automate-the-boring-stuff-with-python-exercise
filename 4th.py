b1b9b1f7dd5c9f85e9197ea265994d8da4da3525
Sergey Bizinow <44430947+biz92@users.noreply.github.com>
2019-01-05 21:49:16 +0300

    Update 4th.py

--- 4th.py
+++ 4th.py
@@ -7,3 +7,23 @@ for i in range(len(my_list) - 1):
     my_str += my_list[i] + ","
 
 print(my_str + 'and ' + my_list[-1] )
+
+
+## Picture
+
+grid = [['.','.','.','.','.','.'],
+        ['.','O','O','.','.','.'],
+        ['O','O','O','O','.','.'],
+        ['O','O','O','O','O','.'],
+        ['.','O','O','O','O','O'],
+        ['O','O','O','O','O','.'],
+        ['O','O','O','O','.','.'],
+        ['.','O','O','.','.','.'],
+        ['.','.','.','.','.','.']]
+
+# print(grid[1][1]) # missed in a book?!
+
+for i in range(6):
+    for j in range(9):
+        print(grid[j][i], end=' ')
+    print(' ')
