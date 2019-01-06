3675ac53a9c42ce961b5c01ecc39c69809da9676
Sergey Bizinow <44430947+biz92@users.noreply.github.com>
2019-01-06 23:12:59 +0300

    Update 5th.py

--- 5th.py
+++ 5th.py
@@ -1,4 +1,14 @@
 inventory = {'Arrow': 13, 'Gold': 10023 , 'Sword' : 10, 'Beer': 1 , 'Coat': 3}
+loot = ['Gold', 'Gold', 'Sword', 'Dagger', 'Ruby', 'Glass' ]
+
+def looting():
+    for i in loot:
+        if i in inventory:
+            inventory[i] = inventory[i] + 1
+        else:
+            inventory.setdefault(i, 0)
+            inventory[i] = inventory[i] + 1
+    return inventory
 
 def displayInv(inv):
     print('Inventory')
@@ -7,6 +17,5 @@ def displayInv(inv):
         print(str(b) + ' ' + a)
         items_total_start += b
     print('Total ' + str(items_total_start))
-displayInv(inventory)
 
-Loot = ['Gold', 'Dagger', 'Gold', 'Sword']
+displayInv(looting())
